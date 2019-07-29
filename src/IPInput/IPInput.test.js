ebaa42e1d29bf35a034767b5b4edfb2289e1bfe3
Andrii Kladochnyi <kladochny.andriy@hotmail.com>
2019-07-29 23:55:47 +0300

    added some change tests

--- src/IPInput/IPInput.test.js
+++ src/IPInput/IPInput.test.js
@@ -63,4 +63,38 @@ describe("IPInput", () => {
         expect(wrapper.at(0).hasClass(errorClass)).toEqual(true);
     });
 
+    it('trigger onChange handler with new value', () => {
+        const oldFirstOcted = 10;
+        const newFirstOcted = 11;
+        const onChangeMock = jest.fn();
+        const wrapper = shallow(<IPInput 
+                                    {...defProps} 
+                                    value={`${oldFirstOcted}.0.0.1`} 
+                                    onChange={onChangeMock}
+                                />);
+
+        wrapper.find("input").at(0).simulate("change", {target: {value: newFirstOcted}})
+        expect(onChangeMock).lastCalledWith(`${newFirstOcted}.0.0.1`);
+    });
+
+    it('don\'t change value if not a number was entered', () => {
+        const oldFirstOcted = "1";
+        const newFirstOcted = "a";
+        const onChangeMock = jest.fn();
+        const preventDefMock = jest.fn();
+        const eventMock = {
+            target: {value: newFirstOcted},
+            preventDefault: preventDefMock
+        };
+        const wrapper = shallow(<IPInput 
+                                    {...defProps} 
+                                    value={`${oldFirstOcted}.0.0.1`} 
+                                    onChange={onChangeMock}
+                                />);
+
+        wrapper.find("input").at(0).simulate("change", eventMock);
+        expect(onChangeMock.mock.calls.length).toEqual(0);
+        expect(preventDefMock.mock.calls.length).toEqual(1);
+    });
+
 });
